27814fd62fe45d3369c62660bbdc9a3d24a9312c
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-05-20 18:31:41 +0900

    Style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <section>
-      <h1>Bookclub</h1>
+    <header className="p-4 flex justify-between items-center bg-brand-dark text-white">
+      <h1 className="p-1 font-bold text-lg">
+        <Link to="/">Bookclub</Link>
+      </h1>
       <nav>
-        <ul>
-          <li>
+        <ul className="flex gap-2 items-center">
+          <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
             <Link to={`bookcase/:uid`}>My Bookcase</Link>
           </li>
-          <li>
+          <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
             <Link to="manage">Manage</Link>
           </li>
-          <li>
+          <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
             <button>SignIn</button>
           </li>
         </ul>
       </nav>
-    </section>
+    </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#7091E6',
+        'brand-dark': '#0d216d',
+        'brand-light': '#8697C4',
+        'brand-accent': '#ADBBDA',
+      },
+    },
   },
   plugins: [],
 };
