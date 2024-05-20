45270982a1cda713dd6ba3e70e7e665b88d54579
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-05-20 18:01:07 +0900

    Create mock data of books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
-  return <div>Home</div>;
+  const [books, setBooks] = useState();
+  useEffect(() => {
+    fetch('data/mockdata.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data.books);
+      });
+  }, []);
+
+  return (
+    <div>
+      Home
+      {books && books.map((book) => <p>{book.volumeInfo.title}</p>)}
+    </div>
+  );
 }
