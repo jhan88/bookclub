0aa21a97601fc12ee1f204f80467a6daddfac4ff
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-05-26 01:29:25 +0900

    Show delete result

--- src/components/DeleteBook.jsx
+++ src/components/DeleteBook.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { deleteBook, readInventory } from '../api/firebase';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import BookCard from './BookCard';
+import { createPortal } from 'react-dom';
 
 export default function DeleteBook() {
+  const queryClient = useQueryClient();
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    queryClient.invalidateQueries(['inventory']);
+  }, [queryClient]);
+
   const { data: inventory } = useQuery({
     queryKey: ['inventory'],
     queryFn: () => readInventory(),
@@ -12,8 +21,17 @@ export default function DeleteBook() {
     refetchOnMount: true,
   });
 
+  const { mutate } = useMutation({
+    mutationFn: (id) => deleteBook(id),
+    onSuccess: () => queryClient.invalidateQueries(['inventory']),
+  });
+
   const handleDelete = (id) => {
-    deleteBook(id).then(() => console.log('Success!'));
+    mutate(id, {
+      onSuccess: () => setMessage('Success!'),
+      onError: () => setMessage('Error!'),
+      onSettled: () => setTimeout(() => setMessage(''), 3000),
+    });
   };
 
   return (
@@ -32,6 +50,13 @@ export default function DeleteBook() {
             </li>
           ))}
       </ul>
+      {message &&
+        createPortal(
+          <p className="fixed bottom-0 p-2 w-full bg-brand text-center text-lg text-white font-semibold opacity-70">
+            {message}
+          </p>,
+          document.body
+        )}
     </section>
   );
 }
