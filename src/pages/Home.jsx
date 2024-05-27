f2da32d31dcfbf693837f1ea563fa43fb2df1c54
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-05-27 16:19:30 +0900

    Display inventory books in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { readInventory } from '../api/firebase';
+import BookCard from '../components/BookCard';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  const [books, setBooks] = useState();
-  useEffect(() => {
-    fetch('data/mockdata.json')
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.books);
-      });
-  }, []);
+  const navigate = useNavigate();
+
+  const { data: inventory } = useQuery({
+    queryKey: ['inventory'],
+    queryFn: () => readInventory(),
+    staleTime: 5 * 60 * 1000,
+  });
 
   return (
-    <div>
-      Home
-      {books && books.map((book) => <p>{book.volumeInfo.title}</p>)}
-    </div>
+    <section>
+      <ul>
+        {inventory &&
+          Object.values(inventory).map((book) => (
+            <li key={book.id} className="p-4">
+              <BookCard book={book} />
+              <button
+                onClick={() => navigate('book/' + book.id)}
+                className="w-full p-1 my-1 rounded text-white bg-brand-light hover:bg-brand"
+              >
+                Review this book
+              </button>
+            </li>
+          ))}
+      </ul>
+    </section>
   );
 }
