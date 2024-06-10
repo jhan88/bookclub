9598b4e0ad23a37defb4c9fad89bd8147bf89143
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-06-10 15:25:20 +0900

    Create useReview hook

---
+++ src/hooks/useReview.jsx
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteReview, isReviewed, submitReview } from '../api/firebase';
+import { useEffect, useState } from 'react';
+
+export default function useReview({ bookId, user }) {
+  const [reviewed, setReviewed] = useState(false);
+
+  useEffect(() => {
+    isReviewed(user.uid, bookId, setReviewed);
+  }, [user, bookId]);
+
+  const reviewId = user.uid + '-review-' + bookId;
+
+  const queryClient = useQueryClient();
+
+  const prevReview = () => {
+    return reviewed
+      ? queryClient.getQueryData(['reviews', bookId])[reviewId]
+      : null;
+  };
+
+  const submit = useMutation({
+    mutationFn: ({ contents, isEditing }) => {
+      submitReview(user, bookId, contents, isEditing);
+    },
+    onSuccess: () => queryClient.invalidateQueries(['reviews', bookId]),
+  });
+
+  const remove = useMutation({
+    mutationFn: (reviewId) => deleteReview(user.uid, bookId, reviewId),
+    onSuccess: () => queryClient.invalidateQueries(['reviews', bookId]),
+  });
+
+  return { reviewed, reviewId, prevReview, submit, remove };
+}
