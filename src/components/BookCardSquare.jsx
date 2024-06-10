aa1f974e69b04399f27b3468b87caf25ebd397b4
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-06-10 18:00:23 +0900

    Implement grid view mode in Home page

---
+++ src/components/BookCardSquare.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function BookCardSquare({ book }) {
+  const { title, authors, thumbnail } = book;
+
+  return (
+    <div className="relative m-auto flex justify-center items-center h-52 w-52 rounded overflow-hidden shadow hover:shadow-lg hover:scale-105">
+      <div className="absolute p-2 flex flex-col justify-around bg-gray-100 h-full w-full transition opacity-0 -translate-y-10 hover:opacity-80 hover:translate-y-0">
+        <h1 className="font-bold line-clamp-3">{title}</h1>
+        {authors && (
+          <p>
+            by <span className="font-semibold text-sm">{authors[0]}</span>
+            {authors.length > 1 ? ' et al.' : ''}
+          </p>
+        )}
+      </div>
+      <img
+        src={thumbnail}
+        alt={'thumbnail of ' + title}
+        className="max-h-48 aspect-[2/3] object-contain"
+      />
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { readInventory } from '../api/firebase';
+import ToggleButton from '../components/ui/ToggleButton';
 import BookCard from '../components/BookCard';
-import { useNavigate } from 'react-router-dom';
+import BookCardSquare from '../components/BookCardSquare';
+import { Link, useNavigate } from 'react-router-dom';
+import { IoListOutline, IoGridOutline } from 'react-icons/io5';
 
 export default function Home() {
+  const [viewMode, setViewMode] = useState('list');
+
   const navigate = useNavigate();
 
   const { data: inventory } = useQuery({
@@ -15,17 +20,40 @@ export default function Home() {
 
   return (
     <section>
-      <ul>
+      <aside className="block text-right">
+        <ToggleButton
+          text={<IoListOutline className="text-2xl" />}
+          handleClick={() => setViewMode('list')}
+          active={viewMode === 'list'}
+        />
+        <ToggleButton
+          text={<IoGridOutline className="text-2xl" />}
+          handleClick={() => setViewMode('grid')}
+          active={viewMode === 'grid'}
+        />
+      </aside>
+      <ul className={(viewMode === 'grid' ? 'grid ' : '') + 'grid-cols-4'}>
         {inventory &&
           Object.values(inventory).map((book) => (
             <li key={book.id} className="p-4">
-              <BookCard book={book} />
-              <button
-                onClick={() => navigate('book/' + book.id, { state: { book } })}
-                className="w-full p-1 my-1 rounded text-white bg-brand-light hover:bg-brand"
-              >
-                Review this book
-              </button>
+              {viewMode === 'list' && (
+                <>
+                  <BookCard book={book} />
+                  <button
+                    onClick={() =>
+                      navigate('book/' + book.id, { state: { book } })
+                    }
+                    className="w-full p-1 my-1 rounded text-white bg-brand-light hover:bg-brand"
+                  >
+                    Review this book
+                  </button>
+                </>
+              )}
+              {viewMode === 'grid' && (
+                <Link to={'/book/' + book.id} state={{ book }}>
+                  <BookCardSquare book={book} />
+                </Link>
+              )}
             </li>
           ))}
       </ul>
