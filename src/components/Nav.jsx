a83d112d19c01d976bbe3ba5807db4a86ca12404
Jiyeon HAN <47693422+jhan88@users.noreply.github.com>
2024-06-13 17:56:45 +0900

    Implement responsive dropdown menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { IoMenu } from 'react-icons/io5';
+import ToggleButton from './ui/ToggleButton';
 
 export default function Nav({ user }) {
+  const [dropdown, setDropdown] = useState(window.innerWidth <= 640);
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  const toggleDropdown = (e) => {
+    setDropdown(e.matches);
+  };
+
+  useEffect(() => {
+    const mql = window.matchMedia('(max-width: 640px)');
+    mql.addEventListener('change', toggleDropdown);
+
+    setOpen(false);
+    return () => mql.removeEventListener('change', toggleDropdown);
+  }, [location]);
+
   return (
-    <nav className="shrink-0">
-      <ul className="flex gap-2 items-center *:shrink-0">
-        <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
-          <Link to="/">Home</Link>
-        </li>
+    <>
+      <div className="visible sm:invisible">
         {user && (
-          <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
-            <Link to={'bookcase/' + user.uid}>My Bookcase</Link>
-          </li>
+          <ToggleButton
+            text={<IoMenu className="text-lg  hover:text-brand-accent" />}
+            handleClick={() => setOpen((prev) => !prev)}
+            active={open}
+          />
         )}
-        {user && user.isAdmin && (
-          <li className="p-1 border-b border-transparent hover:text-brand-accent hover:border-brand-accent">
-            <Link to="manage">Manage</Link>
+      </div>
+      <nav
+        className={
+          user &&
+          (dropdown
+            ? 'absolute w-full py-2 -translate-x-4 bg-inherit transition' +
+              (open
+                ? ' translate-y-24 opacity-90'
+                : ' -translate-y-full opacity-0')
+            : '')
+        }
+      >
+        <ul
+          className={
+            'flex justify-end gap-2 items-center *:shrink-0 *:p-1 *:border-b *:border-transparent hover:*' +
+            (dropdown ? ' flex-col' : ' flex-row')
+          }
+        >
+          <li className="hover:text-brand-accent hover:border-brand-accent">
+            <Link to="/">Home</Link>
           </li>
-        )}
-      </ul>
-    </nav>
+          {user && (
+            <li className="hover:text-brand-accent hover:border-brand-accent">
+              <Link to={'bookcase/' + user.uid}>My Bookcase</Link>
+            </li>
+          )}
+          {user && user.isAdmin && (
+            <li className="hover:text-brand-accent hover:border-brand-accent">
+              <Link to="manage">Manage</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </>
   );
 }
